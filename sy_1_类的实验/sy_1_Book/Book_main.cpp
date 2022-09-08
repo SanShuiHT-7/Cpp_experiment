fd8ca3fe6db2a31f3cc1c4218f8df4671e8b64ae
SanShuiHT-7 <userssht@gmail.com>
2022-09-08 17:55:06 +0800

    完整版实验报告（图床）

--- sy_1_类的实验/sy_1_Book/Book_main.cpp
+++ sy_1_类的实验/sy_1_Book/Book_main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 int main(){
 	Book b1;
-	b1.setName("三体");
-	b1.setPrice(70);
+	b1.setName("西游记");
+	b1.setPrice(30);
 	cout<<"书名："<<"《"<<b1.get_name()<<"》"<<endl;
 	cout<<"价格："<<b1.get_price()<<"元"<<endl; 
 	return 0; 
