6bef94d3d8e5b231d7312dbca5538ad93f9b6faf
SanShuiHT-7 <userssht@gmail.com>
2022-09-10 13:29:15 +0800

    增加了实验二三角形类三条边的合法性判断

--- sy_2_类的进一步应用与编程/sy_2_Tri/Tri_main.cpp
+++ sy_2_类的进一步应用与编程/sy_2_Tri/Tri_main.cpp
@@ -5,15 +5,19 @@ int main()
 {
 	Tri tri1;
 	cout<<"三角形面积为：area1="<<tri1.area()<<endl;
+	cout<<endl;
 	Tri tri2(2,3,4);
 	cout<<"三角形面积为：area2="<<tri2.area()<<endl;
+	cout<<endl;
 	Tri * tri3=new Tri(6,6,6);
 	cout<<"三角形面积为：area3="<<tri3->area()<<endl;
 	delete tri3;
+	cout<<endl;
 	Tri * tri4;
-	tri4=new Tri(12,13,5);
+	tri4=new Tri(10,10,25);
 	cout<<"三角形面积为：area4="<<tri4->area()<<endl;
 	delete tri4;
+	cout<<endl;
 	Tri tri5(tri2);
 	cout<<"使用拷贝构造函数调用输出的三角形面积为：area5="<<tri5.area()<<endl;
 	return 0;
