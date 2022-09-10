49414e6722847ea12c93d70ad15c7de3acecf090
SanShuiHT-7 <userssht@gmail.com>
2022-09-10 16:56:57 +0800

    修改了sy_2_Student的相关错误，题干要求4项详细，我只有3项目

--- sy_2_类的进一步应用与编程/sy_2_Student/Student.h
+++ sy_2_类的进一步应用与编程/sy_2_Student/Student.h
@@ -1,23 +1,26 @@
 #include <iostream>
+#include <iomanip>
 using namespace std; 
 class Student	
 {
 private:
 	string ID;	                                                                            
-	string name; 
+	string name;
+	string sex; 
     float score;    
 
 public:				 
-	Student(string n,string na,float s);	 
+	Student(string n,string na,string se,float s);	 
     void display();          		
 };
-Student::Student(string n,string na,float s)
+Student::Student(string n,string na,string se,float s)
 {
     ID=n;
 	name=na;
+	sex=se;
 	score=s;
 }
 void Student::display()
 {
-    cout<<ID<<","<<name<<","<<score<<endl;
+    cout<<left<<setw(10)<<ID<<setw(10)<<name<<setw(10)<<sex<<setw(10)<<score<<endl;
 }
--- sy_2_类的进一步应用与编程/sy_2_Student/Studnet_main.cpp
+++ sy_2_类的进一步应用与编程/sy_2_Student/Studnet_main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "Student.h"
 using namespace std; 
 int main()
@@ -6,20 +7,22 @@ int main()
 	int i;
     Student Stud[5]=
     {
-    	Student("101","张三",90),
-    	Student("102","李四",91),
-    	Student("103","王五",92),
-    	Student("104","小明",93),
-    	Student("105","小刚",94),
+    	Student("09170301","张三","男",90),
+    	Student("09170302","李四","男",91),
+    	Student("09170303","小夏","女",93),
+    	Student("09170304","王五","男",92),
+    	Student("09170305","小红","女",94),
 	};
 	cout<<"输出5个学生的信息："<<endl;
     Student * p=&Stud[0];
+    cout<<left<<setw(10)<<"学号"<<setw(10)<<"姓名"<<setw(10)<<"性别"<<setw(10)<<"分数"<<endl;
     for(i=0;i<5;i++)
     {
         (p+i)->display();
     };
     cout<<"输出第1、3、5个学生的信息："<<endl;
     Student * p1=&Stud[0];
+    cout<<left<<setw(10)<<"学号"<<setw(10)<<"姓名"<<setw(10)<<"性别"<<setw(10)<<"分数"<<endl;
     for(i=0;i<5;i=i+2)
     {
         (p1+i)->display();
